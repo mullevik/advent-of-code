139b15ceb219153c02dcf21e9cd5570ffd5732ca
Viktor <muller.viktor.muller@gmail.com>
2022-12-02 00:50:16 +0100

    task_01

--- 2022/python/aoc_python/task_01.py
+++ 2022/python/aoc_python/task_01.py
@@ -1,2 +1,18 @@
+from aoc_python.utils import load_lines_as_optional_integers
+
+
 if __name__ == "__main__":
-    print("aoc init")
+    calories_of_elves = load_lines_as_optional_integers("inputs/task_01_1.txt")
+
+    elves_calories = []
+    tmp_buffer = []
+    for calories in calories_of_elves:
+        if calories is None:
+            elves_calories.append([x for x in tmp_buffer])
+            tmp_buffer = []
+        else:
+            tmp_buffer.append(calories)
+    elves_calories.append(tmp_buffer)
+    elves_sums = sorted([sum(x) for x in elves_calories], reverse=True)
+
+    print(elves_sums[0] + elves_sums[1] + elves_sums[2])
