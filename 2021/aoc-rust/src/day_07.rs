c9391c10e228136f8874292bedbc471274dbf016
Viktor Müller <muller.viktor.muller@gmail.com>
2024-06-11 22:40:00 +0200

    Polish bisect search

--- 2021/aoc-rust/src/day_07.rs
+++ 2021/aoc-rust/src/day_07.rs
@@ -1,18 +1,13 @@
-use std::{
-    cmp::{max, min},
-    i64,
-};
 
-use crate::utils::sign;
 
 pub fn first_part(input: &str) -> i32 {
     let crab_positions = parse_input(input);
     // solve_iteratively(&crab_positions, difference_distance)
-    solve_with_gradient_descent(&crab_positions, difference_distance)
+    solve_with_bisect(&crab_positions, difference_distance)
 }
 pub fn second_part(input: &str) -> i32 {
     let crab_positions = parse_input(input);
-    solve_with_gradient_descent(&crab_positions, arithmetic_distance)
+    solve_with_bisect(&crab_positions, arithmetic_distance)
     // solve_iteratively(&crab_positions, arithmetic_distance)
 }
 
@@ -26,33 +21,23 @@ fn solve_iteratively(crab_positions: &[i32], distance_fn: fn(i32, i32) -> i32) -
         .unwrap()
 }
 
-fn solve_with_gradient_descent(crab_positions: &[i32], distance_fn: fn(i32, i32) -> i32) -> i32 {
-    let min_pos = *crab_positions.iter().min().unwrap();
-    let max_pos = *crab_positions.iter().max().unwrap();
+fn solve_with_bisect(crab_positions: &[i32], distance_fn: fn(i32, i32) -> i32) -> i32 {
+    let mut a = *crab_positions.iter().min().unwrap();
+    let mut b = *crab_positions.iter().max().unwrap();
 
-    let mut current_position = 0;
-    let mut step_size = max_pos - min_pos;
-    let mut total_min = i32::MAX;
-    let mut last_total_min_set = 0;
-    loop {
-        let f_0 = count_fuel(current_position, crab_positions, distance_fn);
-        let f_1 = count_fuel(current_position + 1, crab_positions, distance_fn);
-        let grad = f_0 - f_1;
+    let mut f_a = count_fuel(a, crab_positions, distance_fn);
+    let mut f_b = count_fuel(b, crab_positions, distance_fn);
 
-        if f_0 < total_min || f_1 < total_min {
-            total_min = min(f_0, f_1);
+    while a != b {
+        if f_a > f_b {
+            a = (a + 1) + (a.abs_diff(b) / 2) as i32;
+            f_a = count_fuel(a, crab_positions, distance_fn);
         } else {
-            last_total_min_set += 1;
-        }
-
-        current_position = current_position + (sign(grad)) * step_size;
-        step_size = max(step_size / 2, 1);
-
-        if last_total_min_set > 10 {
-            break;
+            b = (b - 1) - (a.abs_diff(b) / 2) as i32;
+            f_b = count_fuel(b, crab_positions, distance_fn);
         }
     }
-    total_min
+    f_a
 }
 
 fn count_fuel(
