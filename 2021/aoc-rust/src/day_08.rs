e53ddeb4a4fedfa50183ee8bd5ca60bd5deb6257
Viktor Müller <muller.viktor.muller@gmail.com>
2024-06-12 22:05:26 +0200

    Complete part one

--- 2021/aoc-rust/src/day_08.rs
+++ 2021/aoc-rust/src/day_08.rs
@@ -2,12 +2,27 @@ use std::{char, collections::HashSet};
 
 pub fn first_part(input: &str) -> i32 {
     let problems = parse(input);
-    -1
+
+    problems
+        .iter()
+        .map(|problem| {
+            problem
+                .output_patterns
+                .iter()
+                .filter(|pattern| {
+                    (pattern.len() == 2)
+                        || (pattern.len() == 4)
+                        || (pattern.len() == 3)
+                        || (pattern.len() == 7)
+                })
+                .count() as i32
+        })
+        .sum::<i32>()
 }
 
 struct Problem {
     signal_patterns: Vec<HashSet<char>>,
-    output_value: HashSet<char>,
+    output_patterns: Vec<HashSet<char>>,
 }
 
 fn parse(input: &str) -> Vec<Problem> {
@@ -20,20 +35,21 @@ fn parse(input: &str) -> Vec<Problem> {
                 .split_whitespace()
                 .map(|part| part.chars().collect::<HashSet<_>>())
                 .collect::<Vec<_>>();
-            let after_pattern = after.chars().collect::<HashSet<_>>();
+            let after_patterns = after
+                .split_whitespace()
+                .map(|part| part.chars().collect::<HashSet<_>>())
+                .collect::<Vec<_>>();
 
             Problem {
                 signal_patterns: before_patterns,
-                output_value: after_pattern,
+                output_patterns: after_patterns,
             }
         })
         .collect::<Vec<Problem>>()
 }
 
 mod test_day_08 {
-
-    use super::{parse, first_part};
-
+    use super::{first_part, parse};
 
     #[test]
     fn test_problem_parsing() {
@@ -43,6 +59,11 @@ mod test_day_08 {
 
     #[test]
     fn test_example() {
-        assert_eq!(first_part(include_str!("../inputs/08_example")), 493);
+        assert_eq!(first_part(include_str!("../inputs/08_example")), 26);
+    }
+
+    #[test]
+    fn test_first_part() {
+        assert_eq!(first_part(include_str!("../inputs/08")), 493);
     }
 }
